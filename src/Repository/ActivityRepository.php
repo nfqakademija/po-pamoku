8a0e1ddfa0506f3ac4f567ce2c5facb83b87b6e5
Deimantas Abelskis <ddeimantass@gmail.com>
2018-04-07 19:09:14 +0300

    Upadte ActivityRepository

--- src/Repository/ActivityRepository.php
+++ src/Repository/ActivityRepository.php
@@ -2,21 +2,21 @@
 
 namespace App\Repository;
 
-use App\Entity\Club;
+use App\Entity\Activity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
- * @method Club|null find($id, $lockMode = null, $lockVersion = null)
- * @method Club|null findOneBy(array $criteria, array $orderBy = null)
- * @method Club[]    findAll()
- * @method Club[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Activity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Activity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Activity[]    findAll()
+ * @method Activity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ActivityRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, Club::class);
+        parent::__construct($registry, Activity::class);
     }
 
 
