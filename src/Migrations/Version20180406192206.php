0c1734915770dcde06b3392086633b56a93964d3
Justina Jankauskaite <jjan.juste@gmail.com>
2018-04-14 10:15:51 +0300

    Modify migrations

--- src/Migrations/Version20180406192206.php
+++ src/Migrations/Version20180406192206.php
@@ -15,7 +15,7 @@ class Version20180406192206 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE club (id INT AUTO_INCREMENT NOT NULL, title TINYTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE IF NOT EXISTS club (id INT AUTO_INCREMENT NOT NULL, title TINYTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema)
