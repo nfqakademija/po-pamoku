f57b62ca71527196293d11aa6a41d409d698c37b
Elenazuokaite <elena.zuokaite@gmail.com>
2018-04-22 22:17:27 +0300

    Add new desing

--- assets/js/App.js
+++ assets/js/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     })
   }
   componentDidMount() {
-    axios.get('/api/activity/1/10')
+    axios.get('/api/activities')
       .then(function (response) {
 
         this.setState({
@@ -34,23 +34,20 @@ class App extends React.Component {
       <div className="container">
         <div className="row activities">
           {isDataArray.length !== 0 ? (isDataArray.map((activity) =>       
-            <div className="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-              <div className="card">
-                <img className="card-img-top" src="https://placeimg.com/640/480/any" alt="Card image cap" />
-                <div className="card-body">
-                  <h5 className="card-title">{activity.name}</h5>
+            <div className="col-md-4 col-sm-6 col-xs-6 py-3">
+              <div className="activity-card">
+                <img className="img-fluid" src="https://placeimg.com/640/480/any" alt="Card image cap" />
+                <div className="activity-text">
+                  <h5 className="activity-title">{activity.name}</h5>
+                  <p>Kaina: {activity.priceFrom}-{activity.priceTo} eur</p>
+                  <p>{activity.city}</p>
+                  <a className="btn btn-more" href={"/activity/" + activity.id}> Plačiau </a>
                 </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">Kaina: {activity.priceFrom} - {activity.priceTo}</li>
-                  <li className="list-group-item">Amžius: {activity.ageFrom} - {activity.ageTo}</li>
-                </ul>
-                <div className="card-body">
-                  <a className="btn btn-primary" href={"/activity/" + activity.id}> Plačiau </a>
-                </div>
-                <button onClick={this.toggleHidden.bind(this)} >
+               
+                {/* <button onClick={this.toggleHidden.bind(this)} >
                   Click to show modal
         </button>
-                {!this.state.isHidden && <Show name="Elena" />}
+                {!this.state.isHidden && <Show name="Elena" />} */}
               </div>
             </div>)) : ('no data')}
         </div>
@@ -59,9 +56,9 @@ class App extends React.Component {
     }
   }
 
-function Show(props) {
-  return <p>Hello, {props.name}</p>;
-}
+// function Show(props) {
+//   return <p>Hello, {props.name}</p>;
+// }
 
 
 export default App;
