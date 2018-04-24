51ea3658211bdfcc4bd29b4610f055d99e55a51b
Elenazuokaite <elena.zuokaite@gmail.com>
2018-04-24 21:59:19 +0300

    Add pagination with React

--- assets/js/App.js
+++ assets/js/App.js
@@ -6,18 +6,21 @@ class App extends React.Component {
     super(props);
     this.state = {
       activities: [],
-      isHidden: true
+      currentPage: 1,
+      activitiesPerPage: 4
     };
+    this.handleClick = this.handleClick.bind(this);
   }
-  toggleHidden() {
+  
+  handleClick(event) {
     this.setState({
-      isHidden: !this.state.isHidden
-    })
+      currentPage: Number(event.target.id)
+    });
   }
+
   componentDidMount() {
     axios.get('/api/activities')
       .then(function (response) {
-
         this.setState({
           activities: Object.keys(response.data).map(i => response.data[i])
         });
@@ -25,40 +28,60 @@ class App extends React.Component {
       .catch(function (error) {
         console.error(error);
       });
-
   }
+
   render() {
-    const isDataArray = this.state.activities;
-    console.log(isDataArray);
+    const { activities, currentPage, activitiesPerPage } = this.state;
+    // pagination logic
+    const indexOfLastActivity = currentPage * activitiesPerPage;
+    const indexOfFirstActivity = indexOfLastActivity - activitiesPerPage;
+    const currentActivities = activities.slice(indexOfFirstActivity, indexOfLastActivity);
+    // ---------------
+     const renderActivities = currentActivities.map((activity, index) => {
+      return <div className="col-md-4 col-sm-6 col-xs-6 py-3">
+      <div className="activity-card">
+        <img className="img-fluid" src="https://placeimg.com/640/480/any" alt="Card image cap" />
+        <div className="activity-text">
+          <h5 key={index} className="activity-title">{activity.name}</h5>
+          <p>Kaina: {activity.priceFrom}-{activity.priceTo} eur</p>
+          <p>{activity.city}</p>
+          <a className="btn btn-more" href={"/activity/" + activity.id}> Plačiau </a>
+        </div>
+      </div>
+  </div>
+    });
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(activities.length / activitiesPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+          <button
+          type="button"
+            key={number}
+            id={number}
+            onClick={this.handleClick}
+          >
+            {number}
+          </button>
+      );
+    });
+
     return (
       <div className="container">
         <div className="row activities">
-          {isDataArray.length !== 0 ? (isDataArray.map((activity) =>       
-            <div className="col-md-4 col-sm-6 col-xs-6 py-3">
-              <div className="activity-card">
-                <img className="img-fluid" src="https://placeimg.com/640/480/any" alt="Card image cap" />
-                <div className="activity-text">
-                  <h5 className="activity-title">{activity.name}</h5>
-                  <p>Kaina: {activity.priceFrom}-{activity.priceTo} eur</p>
-                  <p>{activity.city}</p>
-                  <a className="btn btn-more" href={"/activity/" + activity.id}> Plačiau </a>
-                </div>
-               
-                {/* <button onClick={this.toggleHidden.bind(this)} >
-                  Click to show modal
-        </button>
-                {!this.state.isHidden && <Show name="Elena" />} */}
-              </div>
-            </div>)) : ('no data')}
+           {/* {activities.length !== 0 ? (activities.map((activity) =>  )) : ('no data')} */}
+          {renderActivities}
+        </div>
+        <div id="page-numbers">
+            {renderPageNumbers}
         </div>
       </div>
     );
     }
   }
-
-// function Show(props) {
-//   return <p>Hello, {props.name}</p>;
-// }
 
 
 export default App;
