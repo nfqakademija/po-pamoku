1a878b5c5b456720b255f5774cff54dbdab888c9
Elenazuokaite <elena.zuokaite@gmail.com>
2018-04-27 23:10:38 +0300

    Add pagination via react-bootstrap

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Filter from './components/Filter.js';
+import { Pagination } from '@react-bootstrap/pagination';
 const axios = require('axios');
 
 class App extends React.Component {
@@ -6,78 +8,71 @@ class App extends React.Component {
     super(props);
     this.state = {
       activities: [],
-      currentPage: 1,
-      activitiesPerPage: 4
+      currentPageNumber: 1
     };
-    this.handleClick = this.handleClick.bind(this);
   }
   
-  handleClick(event) {
-    this.setState({
-      currentPage: Number(event.target.id)
-    });
-  }
+ getActivities(page) {
+   axios.get('/api/activity?page=' + page + '&limit=9')
+     .then(function (response) {
+       this.setState({
+         activities: Object.keys(response.data).map(i => response.data[i]),
+         currentPageNumber: page
+       });
+     }.bind(this))
+     .catch(function (error) {
+       console.error(error);
+     });
+}
 
   componentDidMount() {
-    axios.get('/api/activity?page=1&limit=9')
-      .then(function (response) {
-        this.setState({
-          activities: Object.keys(response.data).map(i => response.data[i])
-        });
-      }.bind(this))
-      .catch(function (error) {
-        console.error(error);
-      });
+
+    this.getActivities(1);
+
+  }
+  handleSelect(number) {
+    console.log('handle select', number);
+    this.setState({ currentPageNumber: number });
+    this.getActivities(number);
+  }
+
+
+  onFilterChange(values)  {
+    console.log(values);
   }
 
   render() {
     const { activities, currentPage, activitiesPerPage } = this.state;
-    // pagination logic
-    const indexOfLastActivity = currentPage * activitiesPerPage;
-    const indexOfFirstActivity = indexOfLastActivity - activitiesPerPage;
-    const currentActivities = activities.slice(indexOfFirstActivity, indexOfLastActivity);
-    // ---------------
-     const renderActivities = currentActivities.map((activity, index) => {
-      return <div className="col-md-4 col-sm-6 col-xs-6 py-3">
-      <div className="activity-card">
-        <img className="img-fluid" src="https://placeimg.com/640/480/any" alt="Card image cap" />
-        <div className="activity-text">
-          <h5 key={index} className="activity-title">{activity.name}</h5>
-          <p>Kaina: {activity.priceFrom}-{activity.priceTo} eur</p>
-          <p>{activity.city}</p>
-          <a className="btn btn-more" href={"/activity/" + activity.id}> Plačiau </a>
-        </div>
-      </div>
-  </div>
-    });
-
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(activities.length / activitiesPerPage); i++) {
-      pageNumbers.push(i);
-    }
-
-    const renderPageNumbers = pageNumbers.map(number => {
-      return (
-          <button
-          type="button"
-            key={number}
-            id={number}
-            onClick={this.handleClick}
-          >
-            {number}
-          </button>
-      );
-    });
+    
 
     return (
       <div className="container">
-        <div className="row activities">
-           {/* {activities.length !== 0 ? (activities.map((activity) =>  )) : ('no data')} */}
-          {renderActivities}
+      <Filter 
+        onChange={this.onFilterChange}
+      />
+        <div className="row activities py-3">
+
+           {activities.length !== 0 ? (activities.map((activity, index) =>  
+            <div key={"currentAct" + index} className="col-md-4 col-sm-6 col-xs-6 py-3">
+              <div className="activity-card">
+                <img className="img-fluid" src="https://placeimg.com/640/480/any" alt="Card image cap" />
+                <div className="activity-text">
+                  <h5 className="activity-title">{activity.name}</h5>
+                  <p>Kaina: {activity.priceFrom}-{activity.priceTo} eur</p>
+                  <p>{activity.city}</p>
+                  <a className="btn btn-more" href={"/activity/" + activity.id}> Plačiau </a>
+                </div>
+              </div>
+            </div>)) : ('no data')}
+            
         </div>
-        <div id="page-numbers">
-            {renderPageNumbers}
-        </div>
+
+        <Pagination
+          bsSize="medium"
+          items={6}
+          activepage={this.state.currentPageNumber}
+          onSelect={this.handleSelect.bind(this)} />
+        
       </div>
     );
     }
