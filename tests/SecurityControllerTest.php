2d816bb5eaf886ebe17055331dde4d2c6c14c4f3
Justina Jankauskaitė <jjan.juste@gmail.com>
2018-05-28 23:25:34 +0000

    Remove 'echo' from test

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -24,7 +24,6 @@ class SecurityControllerTest extends WebTestCase
         ]);
 
         $loginSubmit = $client->submit($loginForm);
-        echo $client->getResponse()->getContent();
         $redirect = $client->followRedirect();
         $this->assertContains('Atsijungti', $client->getResponse()->getContent());
 
